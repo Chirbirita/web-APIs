008691b594425680ea722a8e56b5b008625307e0
chirbirita <crisperezarranz@gmail.com>
2023-01-17 19:15:07 +0000

    Refurbish js code to match the instructions in the activity README

--- 02-Stu-Traverse-DOM/starter/assets/js/script.js
+++ 02-Stu-Traverse-DOM/starter/assets/js/script.js
@@ -3,7 +3,11 @@ var articlesDiv = document.getElementById('articles'); // HTML 15 <div id="artic
 var headerDiv = document.getElementById('header');
 
 // Change style by accessing style object's properties
-articlesDiv.children[0].children[1].style.fontSize = '50px'; // 16 - 19 [children 0]; 18 [children 1]
-headerDiv.children[0].style.color = 'white';
+// Targeting 16 - 19 [children 0]; 18 [children 1]
+articlesDiv.children[0].children[1].style.fontSize = '50px';
+//or
+body.children[0].children[1].style.fontSize = '50px'
 
-// Same than line 6 --> body.children[0].children[1].style.fontSize = '50px'
+headerDiv.children[0].style.color = 'white';
+//or same thing:
+document.getElementById("header").children[0].style.color = "white";
