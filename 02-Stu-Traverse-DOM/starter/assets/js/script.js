675024e936e3558aec89aa4907056c25cb76f873
chirbirita <crisperezarranz@gmail.com>
2023-01-17 20:30:32 +0000

    Remove unwanted files and solve activity 02

--- 02-Stu-Traverse-DOM/starter/assets/js/script.js
+++ 02-Stu-Traverse-DOM/starter/assets/js/script.js
@@ -5,8 +5,9 @@ var headerDiv = document.getElementById('header');
 // Change style by accessing style object's properties
 // Targeting 16 - 19 [children 0]; 18 [children 1]
 articlesDiv.children[0].children[1].style.fontSize = '50px';
-//or
-body.children[0].children[1].style.fontSize = '50px'
+
+//or - document.body.children[0].children[1].style.fontSize = '50px'
+
 
 headerDiv.children[0].style.color = 'white';
 //or same thing:
