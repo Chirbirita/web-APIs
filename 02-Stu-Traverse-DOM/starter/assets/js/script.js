cf393194db566e154ac8ae05af61d1bc83622613
chirbirita <crisperezarranz@gmail.com>
2023-01-17 19:14:23 +0000

    Add all files to repo

--- 02-Stu-Traverse-DOM/starter/assets/js/script.js
+++ 02-Stu-Traverse-DOM/starter/assets/js/script.js
@@ -1,7 +1,9 @@
 // Access element using id
-var articlesDiv = document.getElementById('articles');
+var articlesDiv = document.getElementById('articles'); // HTML 15 <div id="articles">
 var headerDiv = document.getElementById('header');
 
 // Change style by accessing style object's properties
-articlesDiv.children[2].style.fontSize = '50px';
-headerDiv.style.color = 'white';
+articlesDiv.children[0].children[1].style.fontSize = '50px'; // 16 - 19 [children 0]; 18 [children 1]
+headerDiv.children[0].style.color = 'white';
+
+// Same than line 6 --> body.children[0].children[1].style.fontSize = '50px'
