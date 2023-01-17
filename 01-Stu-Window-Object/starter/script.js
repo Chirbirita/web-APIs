4af0a418d9c0136fde15c92d01c01a7219622cc6
chirbirita <crisperezarranz@gmail.com>
2023-01-17 19:02:55 +0000

    Add all files to repo

---
+++ 01-Stu-Window-Object/starter/script.js
@@ -0,0 +1,12 @@
+// TODO: What will the following line of code log?
+console.log(window); //
+
+// TODO: What will the following line of code log?
+console.log(window.document); // 
+
+// TODO: What will the following line of code log?
+console.log(document.documentElement); // Every element in the HTML document.
+
+// TODO: What will the following line of code log?
+console.log(document.head); // The head tag and its children like meta title.
+
